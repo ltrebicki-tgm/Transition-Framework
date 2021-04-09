59841ef80baa8b25203aed103c3e79dd2e7d23eb
ltrebicki-tgm <ltrebicki@student.tgm.ac.at>
2021-04-10 00:34:09 +0200

    Visuelle Änderungen - Constraints geändert - Kommentare - Code aufgeräumt

--- app/src/main/java/com/example/transitionsek/MainActivity.java
+++ app/src/main/java/com/example/transitionsek/MainActivity.java
@@ -29,67 +29,86 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     private Scene scene2 = null;
     private Scene scene3 = null;
 
-    private Spinner spinner;
     private String current_choice = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // Spinner Adapter
-        spinner = findViewById(R.id.spinner);
+        Spinner spinner = findViewById(R.id.spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.spinner, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-        spinner.setPrompt("Transition wählen");
-        spinner.setOnItemSelectedListener(this);
-
-
+        spinner.setAdapter(adapter); // Adapter verknüpfen
+        spinner.setPrompt("Transition wählen"); // Überschrift im Dialog
+        spinner.setOnItemSelectedListener(this); // Listener setzen
         // Scene Root sowie Erstellung der Scenes
         ViewGroup sceneRoot = (ViewGroup) findViewById(R.id.scene_root);
         scene_anfang = Scene.getSceneForLayout(sceneRoot, R.layout.scene_anfang, this);
         scene1 = Scene.getSceneForLayout(sceneRoot, R.layout.scene_eins, this);
         scene2 = Scene.getSceneForLayout(sceneRoot, R.layout.scene_zwei, this);
         scene3 = Scene.getSceneForLayout(sceneRoot, R.layout.scene_drei, this);
-
-        scene_anfang.enter();
-
+        scene_anfang.enter(); // Um von Anfang an eine Scene im Framelayout zu haben
     }
 
+    /**
+     * Überschriebene Listener Methode wenn ein Item des Spinners selektiert wird.
+     * Das ausgewählte Item wird in ein Attribut gespeichert, um später die gewünschte Transition ermitteln zu können.
+     * @param parent die AdapterView, wo die Selektierung stattgefunden hat
+     * @param view die View, in der AdapterView benutzt wurde
+     * @param position die Position der View im Adapter
+     * @param id die ID des ausgewählten Items
+     */
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         current_choice = parent.getItemAtPosition(position).toString();
-        //Toast.makeText(getApplicationContext(), current_choice, Toast.LENGTH_SHORT).show();
     }
-
+    /**
+     * Callback Methode die aufgerufen wird, wenn die Auswahl von der View verschwindet.
+     * Dies ist beispielsweise der Fall, wenn der Adapter leer wird.
+     * @param parent die AdapterView, die nun kein Item mehr erhält
+     */
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
-
+        // nicht benötigt
     }
+    /**
+     * Wechselt zu Scene 1 mit der ausgewählten Transition
+     * @param view das View Objekt, das angeklickt wurde
+     */
     public void to_scene_1(View view) {
         Transition transition = get_transition_type();
-        transition.setDuration(1000);
-        Log.d("debug",transition+"");
         TransitionManager.go(scene1, transition);
     }
-
+    /**
+     * Wechselt zu Scene 2 mit der ausgewählten Transition
+     * @param view das View Objekt, das angeklickt wurde
+     */
     public void to_scene_2(View view) {
         Transition transition = get_transition_type();
-        Log.d("debug",transition+"");
         TransitionManager.go(scene2, transition);
     }
-
+    /**
+     * Wechselt zu Scene 3 mit der ausgewählten Transition
+     * @param view das View Objekt, das angeklickt wurde
+     */
     public void to_scene_3(View view) {
         Transition transition = get_transition_type();
-        Log.d("debug",transition+"");
         TransitionManager.go(scene3, transition);
     }
 
+    /**
+     * Wechselt zur Beginn scene mit der ausgewählten Transition
+     * @param view das View Objekt, das angeklickt wurde
+     */
     public void to_scene_beginning(View view) {
         Transition transition = get_transition_type();
-        Log.d("debug",transition+"");
         TransitionManager.go(scene_anfang, transition);
     }
-
+    /**
+     * Ermittelt die gewünschte Transition die im Spinner ausgewählt wurde.
+     * @return eine Subklasse der Transition Klasse, beispielsweise Fade oder Explode
+     */
     public Transition get_transition_type() {
 
         switch(current_choice) {
@@ -102,6 +121,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             case "Explode":
                 return new Explode();
             case "Slide (Random)":
+                // Zufällige Richtung
                 int[] gravity_arr = {Gravity.START, Gravity.END, Gravity.TOP, Gravity.BOTTOM};
                 return new Slide(gravity_arr[new Random().nextInt(gravity_arr.length)]);
         }
